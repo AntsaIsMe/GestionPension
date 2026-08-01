01f1f47d85c31569674cf60c9c467065548d0705
AntsaIsMe <antsajony@gmail.com>
2026-08-01 18:00:26 +0300

    first project

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.example.pension {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires eu.hansolo.tilesfx;
+
+    opens com.example.pension to javafx.fxml;
+    exports com.example.pension;
+}
