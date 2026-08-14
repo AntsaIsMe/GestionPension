1a827bceed81f7bbc75af9555dc10b6b9de6854b
AntsaIsMe <antsajony@gmail.com>
2026-08-14 15:06:49 +0300

    all shit here

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,7 @@ module com.example.pension {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql; // Nécessaire pour PostgreSQL
-    
+
     opens com.example.pension.shared to javafx.fxml;
     opens com.example.pension to javafx.fxml;
     exports com.example.pension;
